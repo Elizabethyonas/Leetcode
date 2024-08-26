0e35ebd27557b4356c977464da82a738040a875d
Elizabethyonas <131787071+Elizabethyonas@users.noreply.github.com>
2024-08-26 22:24:38 +0300

    Create N_ary_post_order.py

---
+++ N_ary_post_order.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        ans = []
+        def dfs(root):
+            for curr in root.children:
+                dfs(curr)
+            ans.append(root.val)
+        dfs(root)
+        return ans
